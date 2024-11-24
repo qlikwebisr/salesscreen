a79c43a449768612796ac4348574c24681062e35
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2024-11-24 16:04:47 +0200

    remove header from tests iframe section and update tenant configuration in settings.js

--- settings.js
+++ settings.js
@@ -9,11 +9,12 @@ export const config = {
         "5b52357b-fed1-4332-a254-77ba2b77f36d",
         "6185bab9-7b9c-421b-9ef0-528b727c15c2"
     ], */
-    tenant: "5ua3eo022stfzwr.eu.qlikcloud.com",
+    tenant: "spdataisreal.eu.qlikcloud.com", 
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
     appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
     sheets: [
-        "6494ecd7-f210-461c-8eea-3f294f947a0e",
-        "dbd9c802-94a9-4171-89e8-f554c8d5dd8a"
+        "91b4024d-d70e-4889-8f0b-2136dc81f269",
+        "UjzmqnP",
+        "kmynHLQ"
     ]
 }
