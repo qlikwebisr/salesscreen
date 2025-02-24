cf46c7efe7293b6590c721c94368969d8d9fcb28
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2025-02-24 13:29:54 +0200

    refactor sheets configuration in settings.js for clarity

--- settings.js
+++ settings.js
@@ -9,15 +9,14 @@ export const config = {
         "5b52357b-fed1-4332-a254-77ba2b77f36d",
         "6185bab9-7b9c-421b-9ef0-528b727c15c2"
     ], */
-    tenant: "spdataisreal.eu.qlikcloud.com", 
+    tenant: "spdataisreal.eu.qlikcloud.com",
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
     appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
     sheets: [
-        "UjzmqnP/state/analysis/hubUrl/%2Fcatalog%3Fspace_filter%3D6486bed0b79404c519f78f2f",
-        "kmynHLQ/state/analysis/hubUrl/%2Fcatalog%3Fspace_filter%3D6486bed0b79404c519f78f2f",
-        "7e5b7248-69b1-4f06-beae-948c951c8d3f/state/analysis/hubUrl/%2Fcatalog%3Fspace_filter%3D6486bed0b79404c519f78f2f"
+        "UjzmqnP",
+        "kmynHLQ",
+        "7e5b7248-69b1-4f06-beae-948c951c8d3f"
     ]
-    // appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
     // sheets: [
     //     "91b4024d-d70e-4889-8f0b-2136dc81f269",
     //     "UjzmqnP",
