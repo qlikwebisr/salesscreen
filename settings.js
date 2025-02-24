293321f4ffe101788a08890df52fd168c7bc73ff
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2025-02-24 12:44:41 +0200

    refactor iframe-container and update sheets configuration in settings.js

--- settings.js
+++ settings.js
@@ -13,8 +13,14 @@ export const config = {
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
     appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
     sheets: [
-        "91b4024d-d70e-4889-8f0b-2136dc81f269",
-        "UjzmqnP",
-        "kmynHLQ"
+        "UjzmqnP/state/analysis/hubUrl/%2Fcatalog%3Fspace_filter%3D6486bed0b79404c519f78f2f",
+        "kmynHLQ/state/analysis/hubUrl/%2Fcatalog%3Fspace_filter%3D6486bed0b79404c519f78f2f",
+        "7e5b7248-69b1-4f06-beae-948c951c8d3f/state/analysis/hubUrl/%2Fcatalog%3Fspace_filter%3D6486bed0b79404c519f78f2f"
     ]
+    // appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
+    // sheets: [
+    //     "91b4024d-d70e-4889-8f0b-2136dc81f269",
+    //     "UjzmqnP",
+    //     "kmynHLQ"
+    // ]
 }
