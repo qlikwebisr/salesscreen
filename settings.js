37eb6e362adcbe12e55b862c600276eadef5dd53
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2025-04-01 20:11:18 +0300

    update appId and sheets configuration in settings.js; comment out previous configurations for clarity

--- settings.js
+++ settings.js
@@ -1,20 +1,30 @@
 //settings file
 export const config = {
     period:60, //in seconds
-    /* tenant: "qisrael.eu.qlikcloud.com",
-    webIntegrationId: "ZGC8NaE0quC-lNBk07VorG5oCwWdN9wU",
-    appId: "81f5be0c-fd0a-43aa-ba54-73afcc934c75",
-    sheets:[
-        "2f19c8f1-93ac-4a33-a603-2ed347f38758",
-        "5b52357b-fed1-4332-a254-77ba2b77f36d",
-        "6185bab9-7b9c-421b-9ef0-528b727c15c2"
-    ], */
     tenant: "spdataisreal.eu.qlikcloud.com",
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
-    appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
+    appId: "a6473db3-5b61-4197-b433-a8bbacd8676b",
     sheets: [
-        "UjzmqnP",
         "kmynHLQ",
-        "7e5b7248-69b1-4f06-beae-948c951c8d3f"
+        "UjzmqnP",
+        "bpqPhH",
+        "b8b0ac18-8072-42c2-b33f-ef97ca3cfece"
     ]
+    // ========================================
+    // appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
+    // sheets: [
+    //     "UjzmqnP",
+    //     "kmynHLQ",
+    //     "7e5b7248-69b1-4f06-beae-948c951c8d3f"
+    // ]
+    // ========================================
+    // qisrael.eu.qlikcloud.com
+    // tenant: "qisrael.eu.qlikcloud.com",
+    // webIntegrationId: "ZGC8NaE0quC-lNBk07VorG5oCwWdN9wU",
+    // appId: "81f5be0c-fd0a-43aa-ba54-73afcc934c75",
+    // sheets:[
+    //     "2f19c8f1-93ac-4a33-a603-2ed347f38758",
+    //     "5b52357b-fed1-4332-a254-77ba2b77f36d",
+    //     "6185bab9-7b9c-421b-9ef0-528b727c15c2"
+    // ],
 }
