1ccb66dc39282e89ed541775689f711e156e806c
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2025-04-02 14:13:13 +0300

    update sheetId values in settings.js for improved configuration

--- settings.js
+++ settings.js
@@ -4,21 +4,25 @@ export const config = {
     tenant: "spdataisreal.eu.qlikcloud.com",
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
     sheets: [
+        //QTD
         {
             "appId": "a6473db3-5b61-4197-b433-a8bbacd8676b",
             "sheetId": "kmynHLQ",
         },
+        //YTD
         {
             "appId": "a6473db3-5b61-4197-b433-a8bbacd8676b",
             "sheetId": "UjzmqnP",
         },
+        //Meeting AM
         {
             "appId": "40a3f48e-e769-4a6b-826e-9b08860becec",
-            "sheetId": "bpqPhH",
+            "sheetId": "85513b68-f5e5-469c-b2ca-0ae8fd274307",
         },
+        //Meeting BDR
         {
             "appId": "40a3f48e-e769-4a6b-826e-9b08860becec",
-            "sheetId": "b8b0ac18-8072-42c2-b33f-ef97ca3cfece",
+            "sheetId": "2c2f3c4d-1c42-46c9-84de-903bee544a79",
         }
        
     ]
