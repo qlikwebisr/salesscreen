19327840293d6560464aba322d77e2cb7ae7efc3
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2025-04-02 10:06:46 +0300

    remove commented-out appId and sheets configuration from settings.js

--- settings.js
+++ settings.js
@@ -22,21 +22,4 @@ export const config = {
         }
        
     ]
-    // ========================================
-    // appId: "97b6233b-2ce6-43c1-9705-fce4897232a1",
-    // sheets: [
-    //     "UjzmqnP",
-    //     "kmynHLQ",
-    //     "7e5b7248-69b1-4f06-beae-948c951c8d3f"
-    // ]
-    // ========================================
-    // qisrael.eu.qlikcloud.com
-    // tenant: "qisrael.eu.qlikcloud.com",
-    // webIntegrationId: "ZGC8NaE0quC-lNBk07VorG5oCwWdN9wU",
-    // appId: "81f5be0c-fd0a-43aa-ba54-73afcc934c75",
-    // sheets:[
-    //     "2f19c8f1-93ac-4a33-a603-2ed347f38758",
-    //     "5b52357b-fed1-4332-a254-77ba2b77f36d",
-    //     "6185bab9-7b9c-421b-9ef0-528b727c15c2"
-    // ],
 }
