207790bc5e8ca4afeae30e0d6ffb957bb6f96898
Naor5674 <62829204+Naor5674@users.noreply.github.com>
2025-11-25 12:03:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,16 +14,6 @@ export const config = {
             "appId": "a6473db3-5b61-4197-b433-a8bbacd8676b",
             "sheetId": "UjzmqnP",
         },
-        //Meeting AM
-        {
-            "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
-            "sheetId": "VgPvswt",
-        },
-        //Meeting BDR- Sales
-        {
-            "appId": "40a3f48e-e769-4a6b-826e-9b08860becec",
-            "sheetId": "af32c3bb-2db5-4385-aaae-5dae153d33f2",
-        },
         //New AM screen
         {
             "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
