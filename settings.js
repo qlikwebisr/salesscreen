c26b93f6a464427cad3643dce859cf366a433bb6
qlikwebisr <64928424+qlikwebisr@users.noreply.github.com>
2025-12-01 19:19:40 +0200

    Restore New Q4 screen configuration in settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,12 @@ export const config = {
     tenant: "spdataisreal.eu.qlikcloud.com",
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
     sheets: [
+         // //New Q4 screen
+        {
+            "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
+            "sheetId": "gYUVs",
+            "background": "https://spdataisreal.eu.qlikcloud.com/api/v1/apps/97b6233b-2ce6-43c1-9705-fce4897232a1/media/files/%D7%AA%D7%9E%D7%95%D7%A0%D7%AA%20%D7%A8%D7%A7%D7%A2.jpg"
+        },
         //QTD
         {
             "appId": "a6473db3-5b61-4197-b433-a8bbacd8676b",
@@ -24,11 +30,11 @@ export const config = {
             "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
             "sheetId": "vjC",
         },
-        //New Q4 screen
-        {
-            "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
-            "sheetId": "gYUVs",
-            "background": "https://spdataisreal.eu.qlikcloud.com/api/v1/apps/97b6233b-2ce6-43c1-9705-fce4897232a1/media/files/%D7%AA%D7%9E%D7%95%D7%A0%D7%AA%20%D7%A8%D7%A7%D7%A2.jpg"
-        }
+        // //New Q4 screen
+        // {
+        //     "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
+        //     "sheetId": "gYUVs",
+        //     "background": "https://spdataisreal.eu.qlikcloud.com/api/v1/apps/97b6233b-2ce6-43c1-9705-fce4897232a1/media/files/%D7%AA%D7%9E%D7%95%D7%A0%D7%AA%20%D7%A8%D7%A7%D7%A2.jpg"
+        // }
     ]
 }
