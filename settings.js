0d72d0643aa3bb14e1cfeab09f35bf44a1c55ef5
Naor5674 <62829204+Naor5674@users.noreply.github.com>
2025-12-14 12:05:39 +0200

    New YTD Screen

--- settings.js
+++ settings.js
@@ -4,20 +4,15 @@ export const config = {
     tenant: "spdataisreal.eu.qlikcloud.com",
     webIntegrationId: "_5d4e5o5y9SyCP-0kUrr7oyZyLpKylvA",
     sheets: [
-        // //New Q4 screen
+        //New Y/Q-TD screen
+        {
+            "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
+            "sheetId": "8f6e1511-bf17-4e00-a846-cb457b909352",
+        }, 
+         // //New Q4 screen
         {
             "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
             "sheetId": "gYUVs",
-        },
-        //QTD
-        {
-            "appId": "a6473db3-5b61-4197-b433-a8bbacd8676b",
-            "sheetId": "kmynHLQ",
-        },
-        //YTD
-        {
-            "appId": "a6473db3-5b61-4197-b433-a8bbacd8676b",
-            "sheetId": "UjzmqnP",
         },
         //New AM screen
         {
@@ -29,10 +24,5 @@ export const config = {
             "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
             "sheetId": "vjC",
         },
-         //New Y/Q-TD screen
-        {
-            "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
-            "sheetId": "8f6e1511-bf17-4e00-a846-cb457b909352",
-        },      
     ]
 }
