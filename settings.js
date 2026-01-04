f166e0f747c8e0fc3799fd5d48bd5a767c0ecbde
Naor5674 <62829204+Naor5674@users.noreply.github.com>
2026-01-04 10:47:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,24 +10,24 @@ export const config = {
             "sheetId": "gYUVs",
         },
         //New Data screen
-      //  {
-      //      "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
-      //      "sheetId": "35ea9b0b-79f8-4012-b7c4-c7ae35ab40a0",
-      //  },
+        {
+            "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
+            "sheetId": "35ea9b0b-79f8-4012-b7c4-c7ae35ab40a0",
+        },
         //New Y/Q-TD screen
         {
             "appId": "97b6233b-2ce6-43c1-9705-fce4897232a1",
             "sheetId": "8f6e1511-bf17-4e00-a846-cb457b909352",
         }, 
         //New AM screen
-      //  {
-      //      "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
-      //      "sheetId": "QaNThq",
-      //  },
+        {
+            "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
+            "sheetId": "QaNThq",
+        },
         //New BDR screen
-      //  {
-      //      "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
-      //      "sheetId": "vjC",
-      //  },
+        {
+            "appId": "feaf2f99-bfde-4f1d-80f8-7aa7e9b9e938",
+            "sheetId": "vjC",
+        },
     ]
 }
